a1c3653fe8ad0054a8e243032e7b2b1ce9b0d0cc
OuimingaLT <OuimingaLT@192.168.1.84>
2016-12-04 19:01:40 +0100

    - int

--- Web_Claims/src/org/bank/controller/ConnexionOfficerServlet.java
+++ Web_Claims/src/org/bank/controller/ConnexionOfficerServlet.java
@@ -14,7 +14,6 @@ public class ConnexionOfficerServlet extends HttpServlet {
         /*
          * À la réception d'une requête GET, simple affichage du formulaire
          */
-        int a;
         this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
     }
 
